4f6281e4966d3e1b3b8ad98dbc0079446c116f7b
Grzegorz <domaradzki.g@gmail.com>
2019-05-17 17:20:33 +0200

    added function to close displayed big photo

--- src/Components/Album/Album.js
+++ src/Components/Album/Album.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 
 import "./Album.css";
 import { getAlbumPromise, getPhotosPromise } from "../../Services";
+import Photo from "./Photo";
 
 class Album extends Component {
   state = {
     albumName: "",
-    photos: []
+    photos: [],
+    activePhoto: false,
+    photoTitle: "",
+    photoSrc: ""
   };
   componentDidMount() {
     const { albumId } = this.props.match.params;
-    console.log(albumId)
+    console.log(albumId);
     Promise.all([getAlbumPromise(albumId), getPhotosPromise(albumId)]).then(
       ([album, photos]) => {
         this.setState({
@@ -21,18 +25,38 @@ class Album extends Component {
     );
   }
 
-  handleShowImage = id =>{
-    console.log(id)
+  handleShowImage = id => {
+    const photo = this.state.photos.find(photo=>id===photo.id);
+    const title = photo.title;
+    const url = photo.url;
+    this.setState({
+      activePhoto: true,
+      photoTitle:title,
+      photoSrc: url
+    });
+  };
+
+  handleCloseImage = () => {
+    this.setState({
+      activePhoto: false,
+      photoTitle: "",
+      photoSrc: ""
+    });
   }
 
   render() {
-    const { photos, albumName } = this.state;
+    const { photos, albumName, activePhoto, photoSrc, photoTitle } = this.state;
     return (
       <div className="album__container">
+      {activePhoto && <Photo close={()=>this.handleCloseImage()} src={photoSrc} title={photoTitle} activePhoto={activePhoto} />}
         <h2 className="album__header">{albumName}</h2>
         <ul>
           {photos.map(photo => (
-            <li onClick={()=>this.handleShowImage(photo.id)} className="album__box" key={photo.id}>
+            <li
+              onClick={() => this.handleShowImage(photo.id)}
+              className="album__box"
+              key={photo.id}
+            >
               <img alt={photo.title} src={photo.thumbnailUrl} />
               <p className="photo__title">{photo.title}</p>
             </li>
--- src/Components/Album/Photo/Photo.js
+++ src/Components/Album/Photo/Photo.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import './Photo.css';
 
-const Photo = props => {
+const Photo = ({src, title, activePhoto, close}) => {
   return (
-    <div className="photo__container">
+    <div className={`photo__container ${activePhoto? 'photo__active' : ''}`}>
     <img alt={title} src={src} />
-
+    <button onClick={close}>Close</button>
     </div>
   );
 }
