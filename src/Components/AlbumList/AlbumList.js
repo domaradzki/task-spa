5228d1a2f1f2a3281987c832e5138acbc2fb3766
Grzegorz <domaradzki.g@gmail.com>
2019-05-17 13:51:47 +0200

    added promiseAll and get Author data to the json with albums

--- src/Components/AlbumList/AlbumList.js
+++ src/Components/AlbumList/AlbumList.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import "./AlbumList.css";
+import { getAlbumsPromise, getUsersPromise } from "../../Services";
 
 class AlbumList extends Component {
+  state ={
+    albums : [],
+    isLoading:false
+  }
+  componentDidMount() {
+    Promise.all([getAlbumsPromise(), getUsersPromise()])
+    .then(([albums, users]) => {
+      albums.map(album => album.author = users.find(user=>album.userId===user.id));
+      this.setState({
+        albums
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.albums)
     return (
       <div className="ablumlist--container">
-        <h2>Albums</h2>
+        <h2 className="ablumlist--header">Albums</h2>
+        <ul>{}</ul>
       </div>
     );
   }
