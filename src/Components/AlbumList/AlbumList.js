de64a9521b7e757dec2af4bd8f3cfa031458883e
Grzegorz <domaradzki.g@gmail.com>
2019-05-17 14:27:36 +0200

    albums displayed ina list as title and author name

--- src/Components/AlbumList/AlbumList.js
+++ src/Components/AlbumList/AlbumList.js
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
+
 import "./AlbumList.css";
 import { getAlbumsPromise, getUsersPromise } from "../../Services";
 
 class AlbumList extends Component {
-  state ={
-    albums : [],
-    isLoading:false
-  }
+  state = {
+    albums: [],
+    isLoading: false
+  };
   componentDidMount() {
-    Promise.all([getAlbumsPromise(), getUsersPromise()])
-    .then(([albums, users]) => {
-      albums.map(album => album.author = users.find(user=>album.userId===user.id));
-      this.setState({
-        albums
-      })
-    })
+    Promise.all([getAlbumsPromise(), getUsersPromise()]).then(
+      ([albums, users]) => {
+        albums.map(
+          album => (album.author = users.find(user => album.userId === user.id))
+        );
+        this.setState({
+          albums
+        });
+      }
+    );
   }
-
   render() {
-    console.log(this.state.albums)
+    const { albums } = this.state;
     return (
-      <div className="ablumlist--container">
-        <h2 className="ablumlist--header">Albums</h2>
-        <ul>{}</ul>
+      <div className="albumlist--container">
+        <h2 className="albumlist--header">Albums</h2>
+        <ul>
+          {albums.map(album => 
+            <li className="album--box" key={album.id}>
+            <h4 className="album--title">{album.title}</h4>
+            <p className="album--author">{album.author.name}</p>
+            </li>
+          )}
+        </ul>
       </div>
     );
   }
