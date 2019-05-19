871221cf6a8e70bed50476a6363d8a684b191701
Grzegorz <domaradzki.g@gmail.com>
2019-05-19 23:37:28 +0200

    component Author filed with data user, and rendering chosen information

--- src/Components/Author/Author.js
+++ src/Components/Author/Author.js
@@ -6,31 +6,50 @@ import { getUserPromise } from "../../Services";
 
 class Author extends Component {
   state = {
-    user: {}
+    user: {},
+    error:null,
+    isLoading:true
   };
   componentDidMount() {
     const { userId } = this.props.match.params;
     getUserPromise(userId).then(user => {
       this.setState({
-        user
+        user,
+        isLoading:false,
       });
-    });
+    },
+    (error)=>{
+      this.setState({
+        error,
+        isLoading:false,
+      });
+    }
+    );
   }
 
   render() {
-    console.log(this.state.user);
-    const { authorId } = this.state;
+    const {user,isLoading} = this.state;
     return (
-      <div className="album__container">
-        <h2>{authorId}</h2>
+      <>
+      {isLoading ? <div>...Loading</div> :
+      <div className="author__container">
+        <h2>{user.name} - Profile</h2>
         <Link to={`/`}>
           <button className="button__back">Back</button>
         </Link>
-        <button onClick={this.handleScrollTop} className="button__up">
-          Up
-        </button>
-        <ul />
-      </div>
+        <div className="author__data">
+        <label>Adress</label>
+        <p>{user.address.street} {user.address.suite}</p>
+        <p>{user.address.zipcode} {user.address.city}</p>
+        </div>
+        <div className="author__data">
+        <label>Contact</label>
+        <p><span>email: </span>{user.email}</p>
+        <p><span>phone: </span>{user.phone}</p>
+        <p><span>website: </span>{user.website}</p>
+        </div>
+      </div>}
+      </>
     );
   }
 }
